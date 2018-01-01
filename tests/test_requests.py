8372872299a1ef9c03ffd3ad2b001317e6d50618
Matt Griswold <grizz@20c.com>
2018-01-01 16:52:07 +0000

    add context manager test

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -3,9 +3,17 @@
 import requests
 import pytest
 
-
-@pytest.RequestsData("req")
-def test_requests(data_json):
+def assert_test0(data):
     res = requests.get('https://example.com/test0')
     assert res.status_code == 200
-    assert data_json.expected == res.json()
+    assert data.expected == res.json()
+
+
+@pytest.RequestsData("req")
+def test_decorator(data_json):
+    assert_test0(data_json)
+
+
+def test_context(data_json):
+    with pytest.RequestsData("req"):
+        assert_test0(data_json)
