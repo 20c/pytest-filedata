e5504d4ae20abc48c4d2bf791869df811136e017
Matt Griswold <grizz@20c.com>
2019-01-17 21:45:05 +0000

    port to Ctl

--- setup.py
+++ setup.py
@@ -2,18 +2,25 @@
 from setuptools import find_packages, setup
 
 
-version = open('facsimile/VERSION').read().strip()
-requirements = open('facsimile/requirements.txt').read().split("\n")
-test_requirements = open('facsimile/requirements-test.txt').read().split("\n")
+def read_file(name):
+    with open(name) as fobj:
+        return fobj.read().strip()
+
+
+LONG_DESCRIPTION = read_file("README.md")
+VERSION = read_file("Ctl/VERSION")
+REQUIREMENTS = read_file("Ctl/requirements.txt").split('\n')
+TEST_REQUIREMENTS = read_file("Ctl/requirements-test.txt").split('\n')
 
 
 setup(
     name='pytest_filedata',
-    version=version,
+    version=VERSION,
     author='20C',
     author_email='code@20c.com',
     description='easily load data from files',
-    long_description='',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     license='LICENSE.txt',
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -30,10 +37,10 @@ setup(
     packages = find_packages(),
     include_package_data=True,
     url='https://github.com/20c/pytest-filedata',
-    download_url='https://github.com/20c/pytest-filedata/%s' % version,
+    download_url='https://github.com/20c/pytest-filedata/archive/{}.zip'.format(VERSION),
 
-    install_requires=requirements,
-    test_requires=test_requirements,
+    install_requires=REQUIREMENTS,
+    test_requires=TEST_REQUIREMENTS,
 
     zip_safe=True,
     entry_points={
