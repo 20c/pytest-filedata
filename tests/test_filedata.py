f401c64f5b381c17be85de2a0c39c4814ca77f62
Matt Griswold <grizz@20c.com>
2024-04-28 20:47:58 -0500

    modernize (#4)
    * drop py up to 3.8, remove future dep
    
    * fix setup.cfg
    
    * fix copypasta
    
    * poetry lock
    
    * update tests
    
    * format

--- tests/test_filedata.py
+++ tests/test_filedata.py
@@ -1,12 +1,13 @@
-# -*- coding: utf-8 -*-
-
-
 import pytest
 
 
 def assert_json(data, parsed):
     # dump in json format for easily adding expected
-    print("echo \\\n'{}'\\\n > {}/{}.expected".format(data.dumps(parsed), data.path, data.name))
+    print(
+        "echo \\\n'{}'\\\n > {}/{}.expected".format(
+            data.dumps(parsed), data.path, data.name
+        )
+    )
     assert data.expected == parsed
     assert data.name
     assert data.path
@@ -17,6 +18,6 @@ def test_json_data(data_json):
     assert_json(data_json, parsed)
 
 
-#with pytest.raises(ValueError):
+# with pytest.raises(ValueError):
 #    def test_nonexistant(data_nonexistant):
 #        pass
